2a0367bbc025ca751c0ed26d5318ef932a48fe58
Emilio Sabatucci <sabatucci@gmail.com>
2019-06-03 14:56:03 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = function(RED) {
+    function Pdf2Image(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+        node.on('input', function(msg) {
+            msg.payload = msg.payload.toLowerCase();
+            node.send(msg);
+        });
+    }
+    RED.nodes.registerType("pdf2image",Pdf2Image);
+}
